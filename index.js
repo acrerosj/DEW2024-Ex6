9ad108dd19582a2a9d48207ae894753019260afe
Andrés Crespo <acrespor75@gmail.com>
2025-01-27 11:17:34 +0000

    projects and tasks

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/api/project', require('./routes/project'));
+app.use('/api/task', require('./routes/task'));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
